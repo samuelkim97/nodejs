795579def0ab00e8ebb0a1f1a634064081477596
Bum-Soo Kim <kbeom00@gmail.com>
2023-10-04 20:31:56 +0900

    done sync pattern

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-console.log("first");
-setTimeout(() => {
-  console.log("second");
-}, 0);
-console.log("third");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+	if (req.url === "/") {
+		res.end("Home Page");
+	}
+	if (req.url === "/about") {
+		res.end("About Page");
+	}
+	res.end("Error Page");
+});
+
+server.listen(5000, () => {
+	console.log("Server Listening on port 5000...");
+});
