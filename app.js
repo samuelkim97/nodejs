350bc27d7a9927a44ddfc2c6d39cc19e6cc451cc
Bum-Soo Kim <kbeom00@gmail.com>
2023-10-06 15:16:40 +0900

    done stream-http-example.js

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const { createReadStream } = require("fs");
+const http = require("http");
+const fs = require("fs");
 
-const stream = createReadStream(
-	"./content/big.txt"
-	// { highWaterMark: 90000 },
-	// { encoding: "utf8" }
-);
-
-stream.on("data", (result) => {
-	console.log(`Received ${result.length} bytes of data`);
-	// console.log(result);
-});
-stream.on("error", (err) => {
-	console.log(err);
-});
+http
+	.createServer((req, res) => {
+		// const text = fs.readFileSync("./content/big.txt", "utf8");
+		// res.end(text);
+		const fileStream = fs.createReadStream("./content/big.txt", "utf8");
+		fileStream.on("open", () => {
+			fileStream.pipe(res);
+		});
+		fileStream.on("error", (err) => {
+			res.end(err);
+		});
+	})
+	.listen(5000);
